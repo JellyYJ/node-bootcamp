43d417b108bfbae690db238dd376217c0ea0d061
JellyYJ <lyj10000h@gmail.com>
2024-04-02 16:46:52 +0100

    Setup project sturcture

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -15,6 +15,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views')); // node will automatically create the correct path
@@ -80,25 +81,7 @@ app.use((req, res, next) => {
 /**
  * Routes
  */
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas',
-  });
-});
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours',
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
---
+++ 4-natours/routes/viewRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const viewController = require('../controllers/viewController');
+
+const router = express.Router();
+
+// ROUTES - originally in app.js file
+router.get('/overview', viewController.getOverview);
+router.get('/tour', viewController.getTour);
+
+module.exports = router;
