704c5d1b1e905b48fe78877269bb0820fb84f440
JellyYJ <lyj10000h@gmail.com>
2024-04-03 14:06:17 +0100

    Fix bug in tourModel for locations

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -100,13 +100,19 @@ const tourSchema = new mongoose.Schema(
       day: Number,
     },
 
-    locations: {
-      type: { type: String, defult: 'Point', enum: ['Point'] },
-      coordinates: [Number],
-      address: String,
-      description: String,
-      day: Number,
-    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
 
     // guides: Array, // embedded
     guides: [
