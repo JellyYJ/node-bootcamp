a350aa887e86a9fb2d7419c250fbf87dc46bb9bb
JellyYJ <lyj10000h@gmail.com>
2024-04-03 10:27:58 +0100

    build tour page - images, reviews

--- 4-natours/controllers/viewController.js
+++ 4-natours/controllers/viewController.js
@@ -15,10 +15,15 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
-    fileds: 'review rating user',
+    fields: 'review rating user',
   });
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
+
   res.status(200).render('tour', {
-    title: 'The Hiker Tour',
+    title: `${tour.name} Tour`,
     tour,
   });
 });
