4a19831c6eb437430b63f7edcc7827a4a7449d76
JellyYJ <lyj10000h@gmail.com>
2024-04-04 11:00:55 +0100

    Implement parcel

--- 4-natours/public/js/login.js
+++ 4-natours/public/js/login.js
@@ -1,5 +1,5 @@
 /* eslintr-disable */
-const login = async (email, password) => {
+export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
@@ -20,11 +20,3 @@ const login = async (email, password) => {
     console.log(err.response.data);
   }
 };
-
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-
-  login(email, password);
-});
