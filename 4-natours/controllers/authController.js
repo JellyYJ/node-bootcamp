0fd4af3cd8de18aae73ff94d7617398173d67fea
JellyYJ <lyj10000h@gmail.com>
2024-04-05 10:06:04 +0100

    Update authController.js

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -67,8 +67,9 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 
   const correct = await user.correctPassword(password, user.password);
-  console.log('user.password:', user.password);
-  console.log('Input:', password);
+  // console.log('user.password:', user.password);
+  // console.log('Input:', password);
+  // console.log(correct);
   if (!correct) {
     return next(new AppError('Incorrect password, please try again', 401));
   }
