6b74e118699ce41ff0ab0ed7c7087a9a9ec42dbe
JellyYJ <lyj10000h@gmail.com>
2024-04-08 11:08:32 +0100

    Implement Multer and first step in uploading user photo

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
 const router = express.Router();
-
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+
+const multer = require('multer');
+const upload = multer({ dest: 'public/img/users' });
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
@@ -17,7 +18,7 @@ router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
 router.get('/me', userController.getMe, userController.getUser);
-router.patch('/updateMe', userController.updateMe);
+router.patch('/updateMe', upload.single('photo'), userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
 
 // Routes only for admins
