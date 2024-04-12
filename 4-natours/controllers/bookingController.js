04f172a76535e3c4fad808c4fdfc3c9024d33e2f
JellyYJ <lyj10000h@gmail.com>
2024-04-12 14:05:35 +0100

    Manipulations towards booking for admin/guide

--- 4-natours/controllers/bookingController.js
+++ 4-natours/controllers/bookingController.js
@@ -1,6 +1,7 @@
 const catchAsync = require('../utils/catchAsync');
 const Tour = require('../models/tourModel');
 const Booking = require('../models/bookingModel');
+const factory = require('./handlerFactory');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.getCheckOutSession = catchAsync(async (req, res, next) => {
@@ -53,3 +54,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
   res.redirect(req.originalUrl.split('?')[0]); // root url: ${req.protocol}://${req.get('host')}
 });
+
+exports.getBooking = factory.getOne(Booking);
+exports.createBooking = factory.createOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
--- 4-natours/routes/bookingRoutes.js
+++ 4-natours/routes/bookingRoutes.js
@@ -1,13 +1,23 @@
 const express = require('express');
-
 const bookingController = require('../controllers/bookingController');
 const authController = require('../controllers/authController');
 const router = express.Router();
 
-router.get(
-  '/checkout-session/:tourId',
-  authController.protect,
-  bookingController.getCheckOutSession
-);
+router.use(authController.protect);
+
+router.get('/checkout-session/:tourId', bookingController.getCheckOutSession);
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+// WITH .route() is for chainable requests
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 module.exports = router;
