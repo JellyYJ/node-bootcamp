befdd2ff44e43d6c1dfb24120b08fa002cc6af22
JellyYJ <lyj10000h@gmail.com>
2024-04-15 16:53:42 +0100

    Implement cors

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -9,6 +9,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,6 +26,17 @@ app.set('views', path.join(__dirname, 'views')); // node will automatically crea
 /**
  * Global Middleware
  */
+// Implement CORS
+// key: Access-Control-Allow-Origin: allowing all app to consume our API
+app.use(cors());
+/* eg: we have our API: api.natours.com, and frontend: natours.com
+app.use(cors({
+  origin:"https://natours.com"
+})) 
+*/
+
+app.options('*', cors());
+
 // Set security HTTP headers
 app.use(helmet());
 
