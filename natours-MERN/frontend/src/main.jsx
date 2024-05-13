075e00e322d3fd8cd361a2ae967e53c5015b4285
JellyYJ <lyj10000h@gmail.com>
2024-05-13 12:44:13 +0100

    NOT WORKING error boundary

--- natours-MERN/frontend/src/main.jsx
+++ natours-MERN/frontend/src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// import { ErrorBoundary } from "react-error-boundary";
+import ErrorBoundary from "./utils/ErrorBoundary";
+
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* <ErrorBoundary> */}
     <App />
+    {/* </ErrorBoundary> */}
   </React.StrictMode>
 );
