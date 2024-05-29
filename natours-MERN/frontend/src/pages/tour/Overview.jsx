7713917a5a31bc0f16cba4d96f34bc0ec84c03e9
JellyYJ <lyj10000h@gmail.com>
2024-05-29 11:11:25 +0100

    Add filter and sorting to Overview page

--- natours-MERN/frontend/src/pages/tour/Overview.jsx
+++ natours-MERN/frontend/src/pages/tour/Overview.jsx
@@ -4,18 +4,31 @@ import { useTours } from "./useTours";
 
 import Empty from "../../components/Empty";
 import Spinner from "../../components/Spinner";
+import { filterTours, sortTours } from "../../utils/tourUtils";
+import { useEffect, useState } from "react";
+import { getToursByDistance } from "../../api/tourApi";
+import TourFilters from "./TourFilters";
 
-const Container = styled.div`
+const Main = styled.div`
   max-width: 130rem;
   margin: 0 auto;
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(30rem, 1fr));
-  gap: 5rem;
-  padding: 3rem;
+  display: flex;
+  flex-direction: column;
 
   @media only screen and (max-width: 1180px) {
     max-width: 100rem;
-    grid-template-columns: repeat(auto-fit, minmax(30rem, 1fr));
+  }
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(35rem, 1fr));
+  gap: 5rem;
+  padding: 3rem 0;
+
+  @media only screen and (max-width: 1180px) {
+    max-width: 100rem;
+    grid-template-columns: repeat(auto-fit, minmax(35rem, 1fr));
   }
   @media only screen and (max-width: 850px) {
     grid-template-columns: 1fr;
@@ -35,19 +48,64 @@ const CardContainer = styled.div`
 `;
 
 function Overview() {
-  const { isPending, tours } = useTours();
+  const { isPending, tours = [] } = useTours();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sortOrder, setSortOrder] = useState("none");
+  const [filterDifficulty, setFilterDifficulty] = useState("all");
+  const [distanceTours, setDistanceTours] = useState([]);
+
+  useEffect(() => {
+    const getUserLocation = () => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          fetchTours(latitude, longitude);
+        },
+        (error) => {
+          console.error("Error getting user location:", error);
+        }
+      );
+    };
+
+    getUserLocation();
+  }, []);
+
+  const fetchTours = async (latitude, longitude) => {
+    const toursByDistance = await getToursByDistance(latitude, longitude);
+    setDistanceTours(toursByDistance);
+  };
+
+  // FILTERS AND SORTING
+  const filteredTours = filterTours(
+    distanceTours.length ? distanceTours : tours,
+    searchQuery,
+    filterDifficulty
+  );
+  const filteredAndSortedTours = sortTours(filteredTours, sortOrder);
 
   if (isPending) return <Spinner />;
   if (!tours) return <Empty resourceName="Tours" />;
 
   return (
-    <Container>
-      {tours.map((tour) => (
-        <CardContainer key={tour.slug}>
-          <TourCard tour={tour} isPending={isPending} />
-        </CardContainer>
-      ))}
-    </Container>
+    <Main>
+      <TourFilters
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        sortOrder={sortOrder}
+        setSortOrder={setSortOrder}
+        filterDifficulty={filterDifficulty}
+        setFilterDifficulty={setFilterDifficulty}
+      />
+
+      <Container>
+        {filteredAndSortedTours.map((tour) => (
+          <CardContainer key={tour.slug}>
+            <TourCard tour={tour} isPending={isPending} />
+          </CardContainer>
+        ))}
+      </Container>
+    </Main>
   );
 }
 
